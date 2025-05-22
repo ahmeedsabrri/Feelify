458ed0f37c92877b7420dc0b744dee6c3f10328a
ahmeedsabrri <sabriahmeed1@gmail.com>
2025-05-22 14:22:32 +0100

    Add ProtectedView for authenticated user access to test if the  endpoints protected

--- api/auth/urls.py
+++ api/auth/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('auth/v1/spotify/callback/',views.SpotifyOauthCallBackView.as_view(), name="oauth_callback"),
+    path('auth/v1/test/',views.ProtectedView.as_view(),name="test_endpoint"),
 ]
--- api/auth/views.py
+++ api/auth/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
-from users.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import SpotifyOauthCallBackSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        return Response({"message": "You are authenticated!"})
 class SpotifyOauthCallBackView(APIView):
     permission_classes = []
     serializer_class = SpotifyOauthCallBackSerializer
