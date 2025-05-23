57f91e68389ce233c2d503edd66b64f7c36bf8d1
ahmeedsabrri <sabriahmeed1@gmail.com>
2025-05-23 08:27:04 +0100

    Implement logout functionality and set secure cookie for access token

--- api/auth/urls.py
+++ api/auth/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('auth/v1/spotify/callback/',views.SpotifyOauthCallBackView.as_view(), name="oauth_callback"),
+    path('auth/v1/logout/', views.LogoutView.as_view(), name="logout"),
     path('auth/v1/test/',views.ProtectedView.as_view(),name="test_endpoint"),
 ]
